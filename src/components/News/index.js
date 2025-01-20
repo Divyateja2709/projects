bd094e5b671dd2f2dda852d72cefdf9a483eba67
Divyateja2709 <indrakantidivyateja@gmail.com>
2025-01-20 14:41:25 +0530

    Upload folder and its files

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -3,17 +3,15 @@ import styled from "styled-components";
 export const Header = styled.h1`
   text-align: center;
   margin-top: 120px;
-  color: #fff;
+  color: #000;  /* Changed to black */
   margin-bottom: 20px;
-  @media screen and (max-width: 425px) {
-    font-size: 30px;
-  }
+  font-size: 30px;
 `;
 
 export const Container = styled.div`
   width: 93%;
-  padding-right: (1.5rem, 0.75rem);
-  padding-left: (1.5rem, 0.75rem);
+  padding-right: 1.5rem;  /* Fixed padding values */
+  padding-left: 0.75rem;
   margin-right: auto;
   margin-left: auto;
 `;
--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,9 @@
-const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
+const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=in"; // Country set to India
 const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
-const API_KEY = process.env.REACT_APP_API_KEY;;
+const API_KEY = "e4bcb08adc05a04804f3d4856c1b37af"; // Your API key
+
 export const endpointPath = (country, category) =>
-  `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
+  `${API_DOMAIN}&category=${category}&lang=te&apikey=${API_KEY}`;  // lang=en for English
+
 export const endpointSearch = (searchQuery) =>
-  `${API_SEARCH_DOMAIN}${searchQuery}&lang=en&apikey=${API_KEY}`;
+  `${API_SEARCH_DOMAIN}${searchQuery}&lang=te&apikey=${API_KEY}`;  // lang=en for English
